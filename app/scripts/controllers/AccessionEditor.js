94066e13e1109413e040795c6a3e6fb4cfd68bfa
Brett <brettatoms@gmail.com>
2013-03-17 20:52:04 -0400

    scaffolding out the accession editor

--- app/scripts/controllers/AccessionEditor.js
+++ app/scripts/controllers/AccessionEditor.js
@@ -100,6 +100,37 @@ angular.module('BaubleApp')
             }
         };
 
+        $scope.sourceSelectOptions = {
+            minimumInputLength: 1,
+
+            formatResult: function(object, container, query) { return object.str; },
+            formatSelection: function(object, container) { return object.str; },
+
+            id: function(obj) {
+                return obj.ref; // use ref field for id since our resources don't have ids
+            },
+
+            // get the list of families matching the query
+            query: function(options){
+                // TODO: somehow we need to cache the returned results and early search
+                // for new results when the query string is something like .length==2
+                // console.log('query: ', options);....i think this is what the
+                // options.context is for
+                Source.query(options.term + '%', function(response){
+                    $scope.families = response.data.results;
+                    if(response.data.results && response.data.results.length > 0)
+                        options.callback({results: response.data.results});
+                });
+            }
+        };
+
+
+        $scope.newSource = function() {
+            $scope.showSourceEditor = true;
+            // $scope.apply(function() {
+            // })
+        }
+
         $scope.close = function() {
             window.history.back();
         }
---
+++ app/scripts/controllers/SourceEdit.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('BaubleApp')
+    .controller('SourceEditCtrl', function ($scope) {
+        $scope.source = {}
+
+        $scope.modalOptions = {
+            backdropFade: true,
+            dialogFade:true
+        };
+
+        $scope.close = function() {
+            console.log('close()');
+            $scope.showSourceEditor = false;
+        }
+
+        $scope.save = function() {
+            console.log('save()');
+        }
+    });
