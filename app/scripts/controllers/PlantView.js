f8439ba2acf201dcbebf314bd527e2ac2aa6ada6
Brett <brettatoms@gmail.com>
2013-04-04 17:00:21 -0400

    - fix plant view

--- app/scripts/controllers/PlantView.js
+++ app/scripts/controllers/PlantView.js
@@ -2,9 +2,9 @@
 
 angular.module('BaubleApp')
     .controller('PlantViewCtrl', function ($scope, $location, globals, Plant) {
-        $scope.plants = globals.selected;
+        $scope.plant = globals.selected;
         Plant.details(globals.selected, function(result) {
-            $scope.plants = result.data;
+            $scope.plant = result.data;
         });
 
         $scope.$on('plant-edit', function(){
