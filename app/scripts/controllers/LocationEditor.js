444836f2fcd7b4da3ef542b827cab59272da69c4
Brett <brettatoms@gmail.com>
2013-04-04 15:45:44 -0400

    - update the location editor

--- app/scripts/controllers/LocationEditor.js
+++ app/scripts/controllers/LocationEditor.js
@@ -1,54 +1,43 @@
 'use strict';
 
 angular.module('BaubleApp')
-    .controller('LocationEditorCtrl', function ($scope, Plant, Location) {
-        $scope.Location = Location;
-        $scope.location = {};
+    .controller('LocationEditorCtrl', function ($scope, globals, Plant, Location) {
+        // isNew is inherited from the NewCtrl if this is a /new editor
+        $scope.location = globals.selected && !$scope.isNew ? globals.selected : {}
 
-        $scope.families = []; // the list of completions
-        $scope.activeTab = "general";
-
-        // get the accessiong details when the selection is changed
-        $scope.$watch('selected', function() {
-            if(! $scope.selected) return;
-            $scope.Location.details($scope.selected, function(result) {
+        // make sure we have the details
+        if($scope.location && angular.isDefined($scope.location.ref)) {
+            Location.details($scope.location, function(result) {
                 $scope.location = result.data;
             });
-        });
+        }
 
-        $scope.selectOptions = {
-            minimumInputLength: 1,
-
-            formatResult: function(object, container, query) { return object.str; },
-            formatSelection: function(object, container) { return object.str; },
-
-            id: function(obj) {
-                return obj.ref; // use ref field for id since our resources don't have ids
-            },
-
-            // get the list of families matching the query
-            query: function(options){
-                // TODO: somehow we need to cache the returned results and early search
-                // for new results when the query string is something like .length==2
-                // console.log('query: ', options);....i think this is what the
-                // options.context is for
-                Plant.query(options.term + '%', function(response){
-                    $scope.families = response.data.results;
-                    if(response.data.results && response.data.results.length > 0)
-                        options.callback({results: response.data.results});
-                });
-            }
-        };
+        $scope.activeTab = "general";
 
         $scope.close = function() {
             window.history.back();
         }
 
+        $scope.alerts = [];
+        $scope.closeAlert = function(index) {
+            $scope.alerts.splice(index, 1);
+        };
+
         // called when the save button is clicked on the editor
         $scope.save = function() {
             // TODO: we need a way to determine if this is a save on a new or existing
             // object an whether we whould be calling save or edit
-            $scope.Location.save($scope.location);
-            $scope.close();
+            Location.save($scope.location, function(response) {
+                if(response.status < 200 || response.status >= 400) {
+                    if(response.data) {
+                        $scope.alerts.push({type: 'error', msg: "Error!\n" + response.data});
+                    } else {
+                        $scope.alerts.push({type: 'error', msg: "Unknown error!"});
+                    }
+                    return;
+                }
+
+                $scope.close();
+            });
         };
     });
