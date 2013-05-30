67a8c62b57e466c1aa7e4f819dde1cdc7c082f2f
Brett <brettatoms@gmail.com>
2013-05-29 22:53:59 -0400

    - better handling of logged in user

--- app/scripts/services/Auth.js
+++ app/scripts/services/Auth.js
@@ -1,35 +1,42 @@
 'use strict';
 
 angular.module('BaubleApp')
-  .factory('Auth', function (User) {
+    .factory('Auth', function (User) {
 
-      var credentialsKey = "credentials";
+        var credentialsKey = "credentials",
+            userKey = "user";
 
-      return {
-          logIn: function(user, password) {
-              // set the temporarily credentials for the request and
-              // remove them if the request fails
-              sessionStorage.setItem(credentialsKey,
-                                     btoa(user + ":" + password));
-              console.log( "creds: ", sessionStorage.getItem("credentials") );
+        return {
+            logIn: function(username, password) {
+                // set the temporarily credentials for the request and
+                // remove them if the request fails
+                sessionStorage.setItem(credentialsKey,
+                                       btoa(username + ":" + password));
+                User.setDepth(2);
+                return User.query(username)
+                    .success(function(data, status, headers, config) {
+                        var user = data.results[0];
+                        sessionStorage.setItem(userKey, JSON.stringify(user));
+                    })
+                    .error(function(response) {
+                        console.log("Could not authorize user: ", username)
+                        sessionStorage.removeItem(credentialsKey);
+                        sessionStorage.removeItem(userKey);
+                    });
+            },
 
-              return User.query(user)
-                  .success(function(response) {
-                      console.log( 'success' );
+            logOut: function() {
+                sessionStorage.removeItem(credentialsKey);
+            },
 
-                  })
-                  .error(function(response) {
-                      console.log( 'error2' );
-                      sessionStorage.removeItem(credentialsKey);
-                  });
+            isLoggedIn: function() {
+                return sessionStorage.getItem(credentialsKey) &&
+                    sessionStorage.getItem(userKey);
+            },
 
-          },
-          logOut: function() {
-              sessionStorage.removeItem(credentialsKey);
-          },
-
-          isLoggedIn: function() {
-              return sessionStorage.getItem(credentialsKey) != null;
-          }
-      };
-  });
+            // return the logged in user record
+            getUser: function() {
+                return JSON.parse(sessionStorage.getItem(userKey)) || null;
+            }
+        };
+    });
