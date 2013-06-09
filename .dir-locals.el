266db2ec2bbc207985df2a57f938327b41225dac
root <brett@localhost>
2013-06-09 15:43:46 -0400

    - add .dirs-local.el to make flycheck work in js-mode

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((js-mode
+  (eval . (setq flycheck-flake8rc (expand-file-name ".jshintrc" (projectile-project-root))))))
