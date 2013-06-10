bd773716ff3adba13abd556a83f786f196565ef1
root <brett@localhost>
2013-06-09 22:08:28 -0400

    - add application wide alerts

--- app/scripts/controllers/App.js
+++ app/scripts/controllers/App.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('BaubleApp')
-    .controller('AppCtrl', function ($scope, $location, Auth) {
+    .controller('AppCtrl', function ($scope, $location, globals, Auth) {
 
         //$scope.shared = {};
         //$scope.shared.hideMainMenu = true;
+        $scope.alerts = globals.alerts;
+        $scope.closeAlert = function(index) {
+            $scope.alerts.splice(index, 1);
+        };
 
         $scope.$on("$routeChangeStart", function(event, nextRoute, currentRoute) {
             if(!Auth.isLoggedIn() && $location.path() !== '/login') {
--- app/scripts/services/globals.js
+++ app/scripts/services/globals.js
@@ -4,7 +4,14 @@ angular.module('BaubleApp')
     .factory('globals', function () {
 
         return {
-            apiRoot: "http://eurystyles:9090/api/v1",
+            apiRoot: 'http://eurystyles:9090/api/v1',
+
+            alerts: [],
+
+            addAlert: function(message, type){
+                type = type || "success";
+                this.alerts.push({ msg: message, type: type});
+            },
 
             getAuthHeader: function() {
                 console.log( 'getAuthHeader()' );
