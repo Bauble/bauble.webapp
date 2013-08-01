d904a73e5b08d18d3973c431c70f689eaa559158
root <brett@localhost>
2013-07-31 22:57:26 -0400

    - lots of markup fixes

--- app/scripts/controllers/Search.js
+++ app/scripts/controllers/Search.js
@@ -4,16 +4,18 @@ angular.module('BaubleApp')
     .controller('SearchCtrl', function ($scope, globals, Search, ViewMeta) {
         $scope.viewMeta = null;
         $scope.selected = null;
-        $scope.results = []; // the results of the search
+        $scope.results = null; // the results of the search
 
         // query the server for search results
         $scope.search = function(q) {
+            $scope.results = [];
             if(!q) {
                 $scope.alert = "Please enter a search query";
                 return;
             } else {
                 $scope.alert = null;
             }
+
             Search.query(q)
                 .success(function(data, status, headers, config) {
                     console.log("data: ", data);
