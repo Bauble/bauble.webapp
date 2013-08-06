662766d316f1f229c994fe0e5f7ab0701b2ee9d7
root <brett@localhost>
2013-08-05 22:25:55 -0400

    - some note editor improvements

--- app/scripts/controllers/NotesEditor.js
+++ app/scripts/controllers/NotesEditor.js
@@ -1,11 +1,22 @@
 'use strict';
 
 angular.module('BaubleApp')
-    .controller('NotesEditorCtrl', function ($scope) {
+    .controller('NotesEditorCtrl', function ($scope, Auth) {
         //$scope.notes = [];
         $scope.addNote = function() {
-            $scope.notes.push({date: new Date()});
+            $scope.notes.push({
+                user: Auth.getUser().username,
+                date: new Date()
+            });
         };
+
+        if($scope.notes.length === 0) {
+            $scope.addNote();
+        }
+
+        $scope.noteTitle = function(note){
+            return [note.user, note.date, note.category].join(' - ');
+        }
 
         $scope.dateOptions = {
 
--- app/scripts/directives/bs-datepicker.js
+++ app/scripts/directives/bs-datepicker.js
@@ -4,7 +4,8 @@ angular.module('BaubleApp')
     .directive('bsDatepicker', function () {
         return {
             template: '<input type="text" class="form-control" class="datepicker">',
-            restrict: 'E',
+            //restrict: 'E',
+            restrict: 'A',
             scope: {
                 model: '=ngModel',
                 options: '='
@@ -14,19 +15,22 @@ angular.module('BaubleApp')
                 //element.text('this is the bsDatepicker directive');
                 console.log("scope: ", scope);
                 console.log("scope.model: ", scope.model);
-                attrs.value = scope.model;
                 scope.$watch(scope.model, function() {
-                    //attrs.value = scope.model;
                     console.log("scope.model: ", scope.model);
+                    element.datepicker('setDate', scope.model);
                 });
 
                 var defaultOptions = {
                     autoclose: true
-                }
+                };
+
                 element.datepicker(angular.extend(defaultOptions, scope.options)).
                     on('changeDate', function(event) {
-                        console.log('changedDate');
-                        scope.model = event.date.toString();
+                        console.log('changedDate: ' + event.date);
+                        // *** This only works if I set the date twice like this???
+                        scope.model = event.date;
+                        //scope.$digest();
+                        scope.$apply('scope.model = event.date;');
                     });
             }
         };
