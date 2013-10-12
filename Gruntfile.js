f337c7281f43c20b266ddaae3e7bce605d30e5cc
Brett <brett@bauble.io>
2013-10-12 06:20:44 -0400

    move karma conf files to the test directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -276,7 +276,7 @@ module.exports = function (grunt) {
         },
         karma: {
             unit: {
-                configFile: 'karma.conf.js',
+                configFile: 'test/karma.conf.js',
                 singleRun: true
             }
         },
