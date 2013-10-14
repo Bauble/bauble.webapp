f6e249e07225b39f421dd314e844ea763ce2e1a2
Brett <brett@bauble.io>
2013-10-13 21:50:42 -0400

    make tests runnable

--- Gruntfile.js
+++ Gruntfile.js
@@ -73,6 +73,7 @@ module.exports = function (grunt) {
             },
             test: {
                 options: {
+                    port: 9001,
                     middleware: function (connect) {
                         return [
                             mountFolder(connect, '.tmp'),
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -5,16 +5,17 @@ basePath = '';
 
 // list of files / patterns to load in the browser
 files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/components/angular/angular.js',
-  'app/components/angular-mocks/angular-mocks.js',
-  'app/components/angular-ui/build/angular-ui.js',
-  'app/components/angular-bootstrap/ui-bootstrap.js',
-  'app/scripts/*.js',
-  'app/scripts/**/*.js',
-  //'test/mock/**/*.js',
-  'test/spec/**/*.js'
+    JASMINE,
+    JASMINE_ADAPTER,
+    '../app/components/angular/angular.js',
+    '../app/components/angular-mocks/angular-mocks.js',
+    // '../app/components/angular-ui/build/angular-ui.js',
+    // '../app/components/angular-bootstrap/ui-bootstrap.js',
+    '../app/lib/angular-bootstrap/ui-bootstrap-tpls-0.7.0-SNAPSHOT.min.js',
+    '../app/scripts/*.js',
+    '../app/scripts/**/*.js',
+    //'test/mock/**/*.js',
+    'spec/**/*.js'
 ];
 
 // list of files to exclude
@@ -28,7 +29,7 @@ reporters = ['progress'];
 port = 8080;
 
 // cli runner port
-runnerPort = 9100;
+runnerPort = 9101;
 
 // enable / disable colors in the output (reporters and logs)
 colors = true;
