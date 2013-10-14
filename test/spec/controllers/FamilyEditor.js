433f21583de7b9c678ba102854d8f9175a087160
Brett <brett@bauble.io>
2013-10-13 23:48:00 -0400

    add some family controller tests

--- test/spec/controllers/FamilyEditor.js
+++ test/spec/controllers/FamilyEditor.js
@@ -6,20 +6,49 @@ describe('Controller: FamilyEditorCtrl', function () {
     beforeEach(module('BaubleApp'));
 
     var FamilyEditorCtrl,
-        scope;
+        scope,
+        globals,
+        $httpBackend;
 
     // Initialize the controller and a mock scope
-    beforeEach(inject(function ($controller) {
+    beforeEach(inject(function (_globals_, $controller, $httpBackend) {
         scope = {};
+        globals = _globals_;
+        globals.apiRoot = "/test"
+        $httpBackend.when('GET', '/test/family/1').respond(
+            {ref: "/family1", family: "TestFamily"});
         FamilyEditorCtrl = $controller('FamilyEditorCtrl', {
             $scope: scope
         });
+
     }));
 
-    // it('should attach a list of awesomeThings to the scope', function () {
-    //   expect(scope.awesomeThings.length).toBe(3);
-    // });
-    if('should use the globally selected item when creating a new family', function() {
+    afterEach(function() {
+        globals.setSelected({});
+    });
 
+
+    describe("with a selected object", function() {
+        beforeEach(inject(function($controller) {
+            globals.setSelected({ref: "/family/1", family: "TestFamily"})
+            FamilyEditorCtrl = $controller('FamilyEditorCtrl', {
+                $scope: scope
+            });
+        }));
+
+        afterEach(function(){
+            globals.setSelected({});
+        })
+
+        it('should use the selected object.', function() {
+            expect(scope.family.ref).toBe(globals.getSelected().ref)
+        });
+    });
+
+
+    describe("with no selection", function() {
+        it('should create a empty family object. ', function() {
+            expect(JSON.stringify(scope.family)).toBe('{}');
+        });
     });
 });
