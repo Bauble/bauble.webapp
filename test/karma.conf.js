7feff5e4ed7a8aec0d58f8af2c8f128440fe2af2
Brett <brett@bauble.io>
2013-10-15 07:07:31 -0400

    update karma version

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,64 +1,59 @@
 // Karma configuration
+// http://karma-runner.github.io/0.10/config/configuration-file.html
 
-// base path, that will be used to resolve files and exclude
-basePath = '..';
+module.exports = function(config) {
+    config.set({
+        // base path, that will be used to resolve files and exclude
+        basePath: '..',
 
-// list of files / patterns to load in the browser
-files = [
-    JASMINE,
-    JASMINE_ADAPTER,
-    'app/components/jquery/jquery.js',
-    'app/components/angular/angular.js',
-    'app/components/angular-mocks/angular-mocks.js',
-    'app/lib/angular-bootstrap/ui-bootstrap-tpls-0.7.0-SNAPSHOT.min.js',
-    'app/components/select2/select2.js',
-    'app/components/angular-ui-select2/src/select2.js',
-    'app/components/bootstrap-datepicker/js/bootstrap-datepicker.js',
-    'app/components/angular-grid/ng-grid-2.0.7.min.js',
-    'app/components/momentjs/min/moment.min.js',
-    'app/scripts/*.js',
-    'app/scripts/**/*.js',
-    //'test/mock/**/*.js',
-    'test/spec/**/*.js'
-];
+        // testing framework to use (jasmine/mocha/qunit/...)
+        frameworks: ['jasmine'],
 
-// list of files to exclude
-exclude = [];
+        // list of files / patterns to load in the browser
+        files: [
+            'app/components/jquery/jquery.js',
+            'app/components/angular/angular.js',
+            'app/components/angular-mocks/angular-mocks.js',
+            'app/lib/angular-bootstrap/ui-bootstrap-tpls-0.7.0-SNAPSHOT.min.js',
+            'app/components/select2/select2.js',
+            'app/components/angular-ui-select2/src/select2.js',
+            'app/components/bootstrap-datepicker/js/bootstrap-datepicker.js',
+            'app/components/angular-grid/ng-grid-2.0.7.min.js',
+            'app/components/momentjs/min/moment.min.js',
+            'app/scripts/*.js',
+            'app/scripts/**/*.js',
+            //'test/mock/**/*.js',
+            'test/spec/**/*.js'
+        ],
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+        // list of files / patterns to exclude
+        exclude: [],
 
-// web server port
-port = 8080;
+        // web server port
+        port: 8080,
 
-// cli runner port
-runnerPort = 9101;
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-//browsers = ['Chrome'];
-browsers = ['PhantomJS'];
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['PhantomJS'],
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false
+    });
+};
