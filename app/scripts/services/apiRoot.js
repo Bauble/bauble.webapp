bf422fad8e5d13afee2fa0ed51f9931469a293b7
Brett <brett@nextglass.co>
2014-03-02 09:17:21 -0500

    update apiRoot to support staging api

--- app/scripts/services/apiRoot.js
+++ app/scripts/services/apiRoot.js
@@ -3,6 +3,20 @@
 angular.module('BaubleApp')
     .factory('apiRoot', ['$location', function($location) {
         // AngularJS will instantiate a singleton by calling "new" on this function
-        return $location.host() === 'bauble.io' ?
-            'http://api.bauble.io/api/v1' : 'http://localhost:9090/api/v1';
+
+        var stagingHosts = ['app-staging.bauble.io'];
+        var productionHosts = ['app.bauble.io'];
+
+        var host = $location.host();
+
+        if(stagingHosts.indexOf(host) !== -1) {
+            //return 'https://api-staging.bauble.io/v1';
+            return 'https://api-baubleio.rhcloud.com/v1';
+        }
+
+        if(productionHosts.indexOf(host) !== -1) {
+            return 'https://api.nextglass.co/v1';
+        }
+
+        return 'http://localhost:9090/v1';
     }]);
