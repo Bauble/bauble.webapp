12188995047fa204306b7c34cf037ef4ed2b4e30
Brett <brett@nextglass.co>
2014-04-05 20:02:12 -0400

    added change password to settings

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -4,4 +4,26 @@ angular.module('BaubleApp')
   .controller('SettingsCtrl', ['$scope', 'User',
     function ($scope, User) {
         $scope.user = User.local();
+
+        $scope.model = {
+            password1: '',
+            password2: '',
+            changedPasswordSuccess: null
+        };
+
+
+        $scope.changePassword = function(password) {
+            // send request with password from form instead of
+            // stored auth token
+            $scope.user.save({id: $scope.user.id, password: password})
+                .success(function(data, status, headers, config) {
+                    $scope.changePasswordSuccess = true;
+                    $scope.model.password1 = '';
+                    $scope.model.password2 = '';
+                })
+                .error(function(data, status, headers, config) {
+                    $scope.changePasswordSuccess = false;
+                });
+
+        };
     }]);
