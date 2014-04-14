bebfc96774b10a4e491559f598334ec3c29ab77b
Brett <brett@nextglass.co>
2014-04-14 12:48:45 -0400

    fixes for org editing and sending invitations

--- app/scripts/services/invitation.js
+++ app/scripts/services/invitation.js
@@ -1,21 +1,20 @@
 'use strict';
 
 angular.module('BaubleApp')
-  .factory('Invitation',
-   ['$http', 'apiRoot',
+  .factory('Invitation', ['$http', 'apiRoot',
     function ($http, apiRoot) {
 
         return {
             get: function(token) {
                 return $http({
-                    url: [apiRoot, 'invitation', token].join('/'),
+                    url: [apiRoot, 'invitations', token].join('/'),
                     method: 'GET'
                 });
             },
 
             accept: function(token, password) {
                 return $http({
-                    url: [apiRoot, 'invitation', token].join('/'),
+                    url: [apiRoot, 'invitations', token].join('/'),
                     method: 'POST',
                     data: {
                         password: password
