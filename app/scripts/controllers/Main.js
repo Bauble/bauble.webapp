c72b8bbe861265712646b4df3c2da6ac418f1b8e
Brett <brett@nextglass.co>
2014-05-07 21:21:22 -0400

    allow routes that don't require login

--- app/scripts/controllers/Main.js
+++ app/scripts/controllers/Main.js
@@ -7,32 +7,15 @@ angular.module('BaubleApp')
         $scope.user = User.local();
         $scope.Alert = Alert;
 
-        // if(!$scope.user) {
-        //     $location.path('/login');
-        // } else if(!$scope.user.organization_id) {
-        //     console.log('new org');
-        //     $location.path('/organization/new');
-        // }
-
         $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
             // controls the user menu
             $scope.user = User.local();
             if(!$scope.user) {
-                $location.path('/login');
+                var path = _.contains(['main.forgot-password', 'main.reset-password'], toState.name) ?
+                    toState.url : '/login';
+                $location.path(path);
             } else if(!$scope.user.organization_id) {
                 $location.path('/organization/new');
             }
         });
-
-        // if(!$scope.user.organization) {
-        //     $location.path($scope.user ? '/dashboard' : '/login');
-        // }
-        // $location.path($scope.user ? '/dashboard' : '/login');
-        // if(!Auth.isLoggedIn()) {
-        //     $location.path("/login");
-        // } else {
-        //     $location.path("/search");
-        // }
-
-
     }]);
