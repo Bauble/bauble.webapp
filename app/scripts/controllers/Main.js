db1174fd7af3a6236126c4288ba13de515d9739d
Brett <brett@nextglass.co>
2014-05-07 21:52:00 -0400

    refix

--- app/scripts/controllers/Main.js
+++ app/scripts/controllers/Main.js
@@ -11,9 +11,9 @@ angular.module('BaubleApp')
             // controls the user menu
             $scope.user = User.local();
             if(!$scope.user) {
-                var path = _.contains(['main.forgot-password', 'main.reset-password'], toState.name) ?
-                    toState.url : '/login';
-                $location.path(path);
+                if(!_.contains(['main.forgot-password', 'main.reset-password'], toState.name)) {
+                    $location.path('/login');
+                }
             } else if(!$scope.user.organization_id) {
                 $location.path('/organization/new');
             }
